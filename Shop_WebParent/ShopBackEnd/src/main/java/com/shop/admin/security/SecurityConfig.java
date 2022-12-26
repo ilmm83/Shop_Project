7b8d4c5150284ca1f87adae9a8e05296bf133152
Smirnov Daniil <daniil09052003@gmail.com>
2022-12-26 12:26:16 +0300

    to track

---
+++ Shop_WebParent/ShopBackEnd/src/main/java/com/shop/admin/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.shop.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests().anyRequest().permitAll()
+                .and()
+                .build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+}
---
+++ Shop_WebParent/ShopBackEnd/src/test/java/com/shop/admin/repository/PasswordEncoderTest.java
@@ -0,0 +1,23 @@
+package com.shop.admin.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTest {
+
+    @Test
+    public void canEncodePassword() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+        String password = "password";
+        String encoded = encoder.encode(password);
+
+        System.out.println(encoded);
+
+        boolean matches = encoder.matches(password, encoded);
+
+        assertThat(matches).isTrue();
+    }
+
+}
