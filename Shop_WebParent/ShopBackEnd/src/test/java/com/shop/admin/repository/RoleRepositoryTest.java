b7def181712f068487e0c2249c8618312e6048f8
Smirnov Daniil <daniil09052003@gmail.com>
2022-12-26 12:36:25 +0300

    fix roles func.

--- Shop_WebParent/ShopBackEnd/src/test/java/com/shop/admin/repository/RoleRepositoryTest.java
+++ Shop_WebParent/ShopBackEnd/src/test/java/com/shop/admin/repository/RoleRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = NONE)
+// @Rollback(false) -- use this if wanna add new roles into db
 class RoleRepositoryTest {
 
     @Autowired
