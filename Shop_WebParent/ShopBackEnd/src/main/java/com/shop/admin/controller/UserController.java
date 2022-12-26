dc6a69c29d300efabd02d901f2005fd1930337db
Smirnov Daniil <daniil09052003@gmail.com>
2022-12-26 11:29:01 +0300

    add functionality to cancel button on sign in and add security dependency

--- Shop_WebParent/ShopBackEnd/src/main/java/com/shop/admin/controller/UserController.java
+++ Shop_WebParent/ShopBackEnd/src/main/java/com/shop/admin/controller/UserController.java
@@ -1,11 +1,5 @@
 package com.shop.admin.controller;
 
-import com.shop.admin.service.UserService;
-import com.shop.model.User;
-import lombok.RequiredArgsConstructor;
-
-import java.lang.ProcessBuilder.Redirect;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +7,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.shop.admin.service.UserService;
+import com.shop.model.User;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
