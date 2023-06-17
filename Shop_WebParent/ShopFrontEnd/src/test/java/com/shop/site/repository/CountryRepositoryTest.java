fed45d6d7d5d1d01a973fa26a19d72c67a18d49c
Smirnov Daniil <119430958+ilmm83@users.noreply.github.com>
2023-06-17 14:02:30 +0300

    test for update method

--- Shop_WebParent/ShopFrontEnd/src/test/java/com/shop/site/repository/CountryRepositoryTest.java
+++ Shop_WebParent/ShopFrontEnd/src/test/java/com/shop/site/repository/CountryRepositoryTest.java
@@ -12,8 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.willReturn;
 
 @ExtendWith(MockitoExtension.class)
@@ -55,14 +54,23 @@ class CountryRepositoryTest {
 
     @Test
     void canUpdateCountry() {
-        var country = repository.findById(1).get();
-        country.setName("Republic of India");
-        country.setCode("IN");
+        // given
+        var expectedCountry = new Country(1, "China", "CN");
+        var expectedResult = Optional.of(expectedCountry);
+        var expectedUpdatedCountry = new Country(1, "Republic of India", "IN");
 
-        var saved = repository.save(country);
+        willReturn(expectedResult).given(repository).findById(1);
+        willReturn(expectedUpdatedCountry).given(repository).save(expectedUpdatedCountry);
 
-        assertThat(saved).isNotNull();
-        assertThat(saved.getName()).isEqualTo("Republic of India");
+        // when
+        var result = repository.findById(1);
+        var updatedCountry = repository.save(expectedUpdatedCountry);
+
+        // then
+        assertFalse(result.isEmpty());
+        assertNotNull(updatedCountry);
+        assertEquals(expectedResult, result);
+        assertEquals(expectedUpdatedCountry, updatedCountry);
     }
 
     @Test
