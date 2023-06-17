286999f226033b540efc85fb29946e0c9d43f61e
Smirnov Daniil <119430958+ilmm83@users.noreply.github.com>
2023-06-17 17:40:56 +0300

    test for listByCategory method

--- Shop_WebParent/ShopFrontEnd/src/test/java/com/shop/site/repository/ProductRepositoryTest.java
+++ Shop_WebParent/ShopFrontEnd/src/test/java/com/shop/site/repository/ProductRepositoryTest.java
@@ -1,29 +1,44 @@
 package com.shop.site.repository;
 
+import com.common.model.Product;
 import com.shop.site.prouduct.ProductRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.Mockito.mock;
 
-@DataJpaTest(showSql = false)
-@AutoConfigureTestDatabase(replace = NONE)
-@Rollback(true)
+@ExtendWith(MockitoExtension.class)
 class ProductRepositoryTest {
 
-    @Autowired
-    private ProductRepository repository;
+    @Spy
+    ProductRepository repository;
+
 
     @Test
     void canListByCategory() {
-        var cats = repository.listByCategory(1L, null, Pageable.unpaged());
+        // given
+        var expectedPageContent = List.of(new Product());
+        var expectedPage = new PageImpl<>(expectedPageContent);
+        var categoryIDMatch = "category-id-match";
+        var pageable = mock(Pageable.class);
 
-        assertThat(cats).isNotNull();
+        willReturn(expectedPage).given(repository).listByCategory(1L, categoryIDMatch, pageable);
+
+        // when
+        var page = repository.listByCategory(1L, categoryIDMatch, pageable);
+
+        // then
+        assertEquals(expectedPage, page);
+        assertEquals(expectedPageContent, page.getContent());
     }
 
     @Test
