74fad7068b1250a5e8e7cb64e2bb6ef57b6468b5
Smirnov Daniil <119430958+ilmm83@users.noreply.github.com>
2023-06-18 13:57:26 +0300

    test for processReaquestForm_success method

--- Shop_WebParent/ShopFrontEnd/src/test/java/com/shop/site/controller/ForgotPasswordControllerTest.java
+++ Shop_WebParent/ShopFrontEnd/src/test/java/com/shop/site/controller/ForgotPasswordControllerTest.java
@@ -1,16 +1,23 @@
 package com.shop.site.controller;
 
 import com.shop.site.customer.CustomerService;
+import com.shop.site.customer.EmailSettingBag;
 import com.shop.site.customer.ForgotPasswordController;
 import com.shop.site.setting.SettingService;
+import com.shop.site.util.Utility;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.*;
 
 public class ForgotPasswordControllerTest {
 
@@ -76,6 +83,65 @@ public class ForgotPasswordControllerTest {
         // then
         assertEquals(expectedViewName, viewName);
         assertEquals(expectedToken, model.getAttribute("token"));
+    }
+
+    @Test
+    void canSaveNewPassword() {
+        // given
+        var password = "pass";
+        var expectedViewName = "redirect:/login";
+        var expectedToken = "token";
+
+        willDoNothing().given(customerService).updateCustomerPassword(expectedToken, password);
+        given(model.getAttribute("token")).willReturn(expectedToken);
+
+        // when
+        var viewName = controller.saveNewPassword(password, expectedToken);
+
+        // then
+        verify(customerService, times(1)).updateCustomerPassword(expectedToken, password);
+        assertEquals(expectedViewName, viewName);
+        assertEquals(expectedToken, model.getAttribute("token"));
+    }
+
+    @Test
+    void canProcessRequestForm_Success() {
+        // given
+        var expectedEmail = "test-email";
+        var expectedViewName = "customers/password/forgot_password_form";
+        var expectedSettings = mock(EmailSettingBag.class);
+        var expectedMessage = "We have sent you a reset password link to your email.";
+        var expectedUrl = "url";
+        var request = mock(HttpServletRequest.class);
+        var utility = mockStatic(Utility.class);
+        var message = mock(MimeMessage.class);
+        var sender = mock(JavaMailSenderImpl.class);
+
+        given(request.getParameter("email")).willReturn(expectedEmail);
+        given(customerService.updateResetPasswordToken(expectedEmail)).willReturn(expectedMessage);
+        given(settingService.getEmailSettings()).willReturn(expectedSettings);
+        given(expectedSettings.getFromAddress()).willReturn("from address");
+        given(expectedSettings.getSenderName()).willReturn("Sender name");
+        given(expectedSettings.getCustomerVerifySubject()).willReturn("verify subject");
+        given(sender.createMimeMessage()).willReturn(message);
+        given(model.getAttribute("message")).willReturn(expectedMessage);
+
+        utility.when(() -> Utility.getSiteURL(request)).thenReturn(expectedUrl);
+        utility.when(() -> Utility.prepareMailSender(expectedSettings)).thenReturn(sender);
+
+        willDoNothing().given(sender).send(message);
+
+        // when
+        var viewName = controller.processRequestForm(request, model);
+
+        // then
+        verify(customerService, times(1)).updateResetPasswordToken(expectedEmail);
+        verify(settingService, times(1)).getEmailSettings();
+        verify(sender, times(1)).send(message);
+        assertEquals(expectedViewName, viewName);
+        assertEquals(expectedMessage, model.getAttribute("message"));
+
+        utility.close();
     }
 
 }
