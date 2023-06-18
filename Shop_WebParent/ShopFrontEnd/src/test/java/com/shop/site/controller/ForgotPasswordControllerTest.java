171c85bbabd4fd82f4007135e01a1005e034bded
Smirnov Daniil <119430958+ilmm83@users.noreply.github.com>
2023-06-18 14:39:53 +0300

    test for processReaquestForm_fail method

--- Shop_WebParent/ShopFrontEnd/src/test/java/com/shop/site/controller/ForgotPasswordControllerTest.java
+++ Shop_WebParent/ShopFrontEnd/src/test/java/com/shop/site/controller/ForgotPasswordControllerTest.java
@@ -5,6 +5,7 @@ import com.shop.site.customer.EmailSettingBag;
 import com.shop.site.customer.ForgotPasswordController;
 import com.shop.site.setting.SettingService;
 import com.shop.site.util.Utility;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,12 +13,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.ui.Model;
 
+import java.io.UnsupportedEncodingException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.*;
 
 public class ForgotPasswordControllerTest {
 
@@ -144,4 +147,49 @@ public class ForgotPasswordControllerTest {
         utility.close();
     }
 
+    @Test
+    void canProcessRequestForm_Fail() throws MessagingException, UnsupportedEncodingException {
+        // given
+        var expectedEmail = "test-email";
+        var expectedSettings = mock(EmailSettingBag.class);
+        var expectedMessage = "We have sent you a reset password link to your email.";
+        var expectedError = "Could not send an email.";
+        var expectedViewName = "customers/password/forgot_password_form";
+        var expectedUrl = "url";
+        var request = mock(HttpServletRequest.class);
+        var utility = mockStatic(Utility.class);
+        var message = mock(MimeMessage.class);
+        var sender = mock(JavaMailSenderImpl.class);
+        var helper = mock(MimeMessageHelper.class);
+
+        given(request.getParameter("email")).willReturn(expectedEmail);
+        given(customerService.updateResetPasswordToken(expectedEmail)).willReturn(expectedMessage);
+        given(settingService.getEmailSettings()).willReturn(expectedSettings);
+        given(expectedSettings.getSenderName()).willReturn("Sender name");
+        given(expectedSettings.getCustomerVerifySubject()).willReturn("verify subject");
+        given(expectedSettings.getFromAddress()).willReturn("From address");
+        given(sender.createMimeMessage()).willReturn(message);
+        given(model.getAttribute("message")).willReturn(expectedMessage);
+        given(model.getAttribute("error")).willReturn(expectedError);
+
+        utility.when(() -> Utility.getSiteURL(request)).thenReturn(expectedUrl);
+        utility.when(() -> Utility.prepareMailSender(expectedSettings)).thenReturn(sender);
+
+        willThrow(new MessagingException()).given(helper).setFrom(anyString(), anyString());
+        willDoNothing().given(sender).send(message);
+
+        // when
+        var viewName = controller.processRequestForm(request, model);
+
+        // then
+        verify(customerService, times(1)).updateResetPasswordToken(expectedEmail);
+        verify(settingService, times(1)).getEmailSettings();
+        verify(sender, times(1)).send(message);
+        assertEquals(expectedMessage, model.getAttribute("message"));
+        assertEquals(expectedError, model.getAttribute("error"));
+        assertEquals(expectedViewName, viewName);
+        assertThrows(MessagingException.class, () -> helper.setFrom(expectedSettings.getFromAddress(), expectedSettings.getSenderName()));
+
+        utility.close();
+    }
 }
